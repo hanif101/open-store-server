1ce22a50ed20e9c5e2cbac97b28e1860d71c3599
hanif <h.ehmedov@gmail.com>
2022-02-14 00:43:42 -0500

    env env setup db

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ const app = express()
 app.use(
   cors({
     origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
-    // methods: ['GET', 'POST', 'PATCH'],
     credentials: true
   })
 )

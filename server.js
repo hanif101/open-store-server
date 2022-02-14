d20ed4ef154f58f4a8f3f35f29ff51654a13b9d2
hanif <h.ehmedov@gmail.com>
2022-02-14 10:02:17 -0500

    cors issues

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ const serverDevPort = 3040
 const clientDevPort = 7165
 const port = process.env.PORT || serverDevPort
 
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+  // intercept OPTIONS method
+  next()
+}
+
 // database connection
 mongoose
   .connect(db, {
@@ -39,6 +48,10 @@ mongoose
 // app & server created
 const app = express()
 
+//  CORS imported later
+app.use(allowCrossDomain)
+app.use(express.methodOverride())
+
 // cors
 app.use(
   cors({
@@ -47,7 +60,6 @@ app.use(
   })
 )
 
-// app.use()
 // register passport authentication middleware
 
 app.use(auth)
